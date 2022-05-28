6090e7a24e1794bdded2f74196a322ee81308180
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2022-05-28 20:51:37 +0700

    feat: menambahkan contoh sederhana scraper dengan sebuah server

---
+++ 003-simple-scraper-server/server.js
@@ -0,0 +1,159 @@
+const { Cluster } = require("puppeteer-cluster");
+const express = require("express");
+
+const app = express();
+
+(async () => {
+  const scraperCluster = await Cluster.launch({
+    concurrency: Cluster.CONCURRENCY_CONTEXT,
+    maxConcurrency: 2,
+  });
+  await scraperCluster.task(async ({ page, data: { uPIN } }) => {
+    await page.goto(
+      `https://kartuujian.sman12-bekasi.sch.id/cetakskl.php?nisn=${uPIN}`,
+      {
+        waitUntil: "networkidle2",
+      }
+    );
+
+    const table = await page.$$("table");
+    if (table.length < 1)
+      throw new Error(`Tidak ada kartu dengan U-PIN: ${uPIN}`);
+
+    await page.emulateMediaType("print");
+    const pdfBuffer = await page.pdf({
+      format: "A4",
+      scale: 0.85,
+    });
+
+    const dataKartuDigital = await page.evaluate(() => {
+      const jadwalRef = [...document.querySelectorAll('td[rowspan="4"]')];
+      const [nama, kelas, nomorPeserta, npsn, username, password] = [
+        ...document.querySelectorAll('table[width="800"]:nth-of-type(2) td b'),
+      ]
+        .filter((el) => el.parentElement.getAttribute("colspan") === "2")
+        .map((el) => el.innerText.trim());
+
+      const jadwal = jadwalRef.map((j) => {
+        let data = [];
+        let currentElement;
+
+        const parent = j.parentElement;
+
+        while (true) {
+          const next =
+            data.length < 1
+              ? parent.nextElementSibling
+              : currentElement.nextElementSibling;
+
+          if (!next || next.querySelector('td[rowspan="4"]')) break;
+
+          if (!next.querySelector("td").innerText.includes("-")) {
+            const mapelElement = next.querySelector("td");
+            const tokenElement = next.querySelector("td:nth-child(2)");
+            const waktuElement = next.querySelector("td:nth-child(3)");
+
+            const pelajaran = mapelElement.innerText.replace(/[0-9].\s/, "");
+            const tokenStr = tokenElement.innerText;
+            const token = tokenStr.includes("/")
+              ? tokenStr.split(" / ")
+              : [tokenStr];
+            const waktu = waktuElement.innerText.replace("–", "-");
+
+            data.push({
+              pelajaran,
+              token,
+              waktu,
+            });
+          }
+
+          currentElement = next;
+        }
+
+        const hariElement = parent.querySelector("th");
+        const [hari, tanggal] = hariElement.innerText.split(", ");
+
+        return {
+          hari,
+          tanggal,
+          mapel: data,
+        };
+      });
+
+      return {
+        user: {
+          nama,
+          kelas,
+          nomor_peserta: nomorPeserta,
+          npsn,
+          username,
+          password,
+        },
+        jadwal,
+      };
+    });
+
+    return { json: dataKartuDigital, buffer: pdfBuffer };
+  });
+
+  app.get("/", (req, res) =>
+    res.send(`Untuk mendapatkan file pdf, kunjungi /kartu-digital/<U-PIN>
+    
+    Jika ingin mendapatkan kartu dalam bentuk json, kunjungi /json/<U-PIN>`)
+  );
+
+  app.get("/kartu-digital/:upin", async function (req, res) {
+    if (!req.params.upin)
+      return res.json({ error: "Diperlukan sebuah U-PIN!" });
+
+    if (req.params.upin.length <= 5)
+      return res.json({
+        error: "Panjang minimal dari U-PIN adalah 6 karakter!",
+      });
+
+    try {
+      const digitalCard = await scraperCluster.execute({
+        uPIN: req.params.upin,
+      });
+
+      const concatedBuffer = Buffer.concat([
+        digitalCard.buffer,
+        Buffer.from(JSON.stringify(digitalCard.json)),
+      ]);
+
+      res.setHeader(
+        "Content-disposition",
+        `attachment; filename=${digitalCard.json.user.npsn}_${digitalCard.json.user.nama}_${digitalCard.json.user.nomor_peserta}.pdf`
+      );
+
+      res.contentType("application/pdf");
+      res.send(concatedBuffer);
+    } catch (err) {
+      res.json({ error: err.message });
+    }
+  });
+
+  app.get("/json/:upin", async function (req, res) {
+    if (!req.params.upin)
+      return res.json({ error: "Diperlukan sebuah U-PIN!" });
+
+    if (req.params.upin.length <= 5)
+      return res.json({
+        error: "Panjang minimal dari U-PIN adalah 6 karakter!",
+      });
+
+    try {
+      const digitalCard = await scraperCluster.execute({
+        uPIN: req.params.upin,
+      });
+
+      res.json(digitalCard.json);
+    } catch (err) {
+      res.json({ error: err.message });
+    }
+  });
+
+  app.listen(3000, function () {
+    console.log("Scraper server listening on port 3000.");
+  });
+})();
